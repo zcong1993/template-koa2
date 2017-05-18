69c1a2af45cedbe99cf747b148e0fad4b80b3816
zcong1993 <1005997919@qq.com>
2017-05-18 14:50:51 +0800

    tweak *⃣

--- template/router/index.js
+++ template/router/index.js
@@ -6,7 +6,7 @@ const session = require('../controllers/session')<% } %><% } %>
 
 const router = new Router()<% if (!simple) { %>
 
-router.get('/', <% if (!simple) { %>index.index<% } else { %>api.index<% } %>)
+router.get('/', <%= simple ? "api.index":"index.index" %>)
 
 router.post('/echo', api.echo)<% if (!simple) { %>
 
